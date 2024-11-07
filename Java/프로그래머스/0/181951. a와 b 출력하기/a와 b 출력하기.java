29ffa8accf801bcb1ff0588a0da4ff686c634552
kju0811 <rlawldns60@gmail.com>
2024-11-07 21:37:20 +0900

    [level 0] Title: a와 b 출력하기, Time: 436.13 ms, Memory: 70 MB -BaekjoonHub

--- Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
+++ Java/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.java
@@ -6,6 +6,10 @@ public class Solution {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
+        if( a < -100000 || b > 100000) {
+            System.out.print("범위 초과");
+        }
+        
         System.out.println("a = " + a);
         System.out.print("b = " + b);
        
