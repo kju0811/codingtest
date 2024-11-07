cc32e53c328c2ebc75c35306b3a7d5aa18ee80df
kju0811 <rlawldns60@gmail.com>
2024-11-07 21:33:04 +0900

    [level 0] Title: 문자열 출력하기, Time: 149.27 ms, Memory: 62.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
+++ Java/프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -4,6 +4,9 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
+        if (a.length() < 1 || a.length() > 1000000) {
+            System.out.print("str 길이 초과");
+        }
         System.out.print(a);
     }
 }
