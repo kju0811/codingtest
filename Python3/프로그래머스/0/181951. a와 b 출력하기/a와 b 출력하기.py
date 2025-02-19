ffa9575b8243d3ec279b874899c5b2fa0b6adaad
kju0811 <rlawldns60@gmail.com>
2025-02-19 18:16:34 +0900

    [level 0] Title: a와 b 출력하기, Time: 21.30 ms, Memory: 7.51 MB -BaekjoonHub

--- Python3/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.py
+++ Python3/프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.py
@@ -1,7 +1,9 @@
 a, b = map(int, input().strip().split(' '))
 
-if -1000000 <= a <= 1000000 and -1000000 <= b <= 1000000:
-    print(f'a = {a} \nb = {b}')
-else:
-    print("오류")
+while True:
+    if -1000000 <= a and b <= 1000000:
+        print(f'a = {a} \nb = {b}')
+        break
+    else:
+        continue
     
