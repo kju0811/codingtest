d323c2f52a21d76fdc1b32daed1e99c013b5e59c
kju0811 <rlawldns60@gmail.com>
2025-04-15 17:52:50 +0900

    [level 0] Title: 세 개의 구분자, Time: 18.46 ms, Memory: 20.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -0,0 +1,12 @@
+def solution(myStr):
+    s = myStr.replace('b', 'a')
+    s = s.replace('c', 'a')
+    s = s.split('a')
+    answer = []
+    
+    for x in s:
+        if x:
+            answer.append(x)
+    if not answer:
+        return ['EMPTY']
+    return answer
